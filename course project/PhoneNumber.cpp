bacb26df3e10dd6031bbe7fff567c3f6116435f7
DOLGIY-DMITRIY <dolgiy.dmitriyp@gmail.com>
2021-11-14 15:44:10 +0200

    Fuck C++ and Bjarne Stroustrup

---
+++ course project/PhoneNumber.cpp
@@ -0,0 +1,114 @@
+#include "PhoneNumber.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+
+PhoneNumber::PhoneNumber() {
+	fName.open("name.txt");
+	fSurname.open("surname.txt");
+	fPatronymic.open("patronymic.txt");
+	fcellOperator.open("cellOperator.txt");
+
+	int randName = rand() % 20 + 1;
+	for (int i = 0; i < randName; i++)
+	{
+		fName >> name;
+	}
+	int randSurname = rand() % 20 + 1;
+	for (int i = 0; i < randSurname; i++)
+	{
+		fSurname >> surname;
+	}
+	int randPatronymic = rand() % 20 + 1;
+	for (int i = 0; i < randPatronymic; i++)
+	{
+		fPatronymic >> patronymic;
+	}
+
+	randOperator = (rand() % 10) % 3 + 1;
+	for (int i = 0; i < randOperator; i++) {
+		fcellOperator >> cellOperator;
+	}
+
+	switch (randOperator) {
+	case 1: operatorN = funcRandOperator(kyivstar, operatorN);
+		break;
+	case 2: operatorN = funcRandOperator(vodafon, operatorN);
+		break;
+	case 3: operatorN = funcRandOperator(life, operatorN);
+		break;
+	}
+	first = rand() % 900 + 100;
+	int i = 1000;
+	while (i >= 1) {
+		last += rand() % 10 * i;
+		i /= 10;
+	}
+
+	fName.close();
+	fSurname.close();
+	fPatronymic.close();
+	fcellOperator.close();
+
+	sumOneNumb();
+}
+
+void PhoneNumber::print() {
+	cout << num << ". " << surname << " " << name << " " << patronymic << endl
+		<< cellOperator << endl
+		<< "0" << operatorN << first << last << endl
+		<< "Сумма номера: " << sumOne << "\n\n";
+}
+
+void PhoneNumber::newNumber() {
+	if (previous == 0)
+	{
+		num = 1;
+	}
+	else {
+		num = previous->num + 1;
+	}
+}
+
+string PhoneNumber::printInFile() {
+	return to_string(num) + ". " + surname + " " + name + " " + patronymic + "\n"
+		+ cellOperator + "\n"
+		+ "0" + to_string(operatorN) + to_string(first) + to_string(last) + "\n"
+		+ "Сумма номера: " + to_string(sumOne);
+}
+
+string PhoneNumber::getName() {
+	return name;
+}
+
+string PhoneNumber::getSurname() {
+	return surname;
+}
+
+string PhoneNumber::getPatronymic() {
+	return patronymic;
+}
+
+string PhoneNumber::getCellOperator() {
+	return cellOperator;
+}
+
+int PhoneNumber::funcRandOperator(int mas[], int operatorN) {
+	int randChs;
+	randChs = rand() % 3;
+	for (int i = 0; i <= randChs; i++) {
+		operatorN = mas[i];
+	}
+	return operatorN;
+}
+
+void PhoneNumber::sumOneNumb() {
+	numberStr = to_string(operatorN) + to_string(first) + to_string(last);
+	for (int i = 0; i < numberStr.length(); i++) {
+		sumOne += numberStr.at(i) - '0';
+	}
+}
--- course project/PhoneNumber.h
+++ course project/PhoneNumber.h
@@ -1,8 +1,13 @@
 #pragma once
 #include <iostream>
-#include <string>
 #include <fstream>
+#include <string>
+#include <conio.h>
 #include <windows.h>
+
+#ifdef PHONENUMBER_HPP
+#endif // PHONENUMBER_HPP
+#define PHONENUMBER_HPP
 
 using namespace std; 
 
@@ -22,112 +27,19 @@ public:
 	PhoneNumber* next = 0, * previous = 0;
 	static int sumAll;
 
-private:
-	int funcRandOperator(int mas[], int operatorN) {
-		int randChs;
-		randChs = rand() % 3;
-		for (int i = 0; i <= randChs; i++) {
-			operatorN = mas[i];
-		}
-		return operatorN;
-	}
-
-	void sumOneNumb() {
-		numberStr = to_string(operatorN) + to_string(first) + to_string(last);
-		for (int i = 0; i < numberStr.length(); i++) {
-			sumOne += numberStr.at(i) - '0';
-		}
-	}
-
 public:
-	PhoneNumber() {
-		fName.open("name.txt");
-		fSurname.open("surname.txt");
-		fPatronymic.open("patronymic.txt");
-		fcellOperator.open("cellOperator.txt");
+	PhoneNumber();
+	void print();
+	void newNumber();
+	string printInFile();
+	string getName();
+	string getSurname();
+	string getPatronymic();
+	string getCellOperator();
 
-		int randName = rand() % 20 + 1;
-		for (int i = 0; i < randName; i++)
-		{
-			fName >> name;
-		}
-		int randSurname = rand() % 20 + 1;
-		for (int i = 0; i < randSurname; i++)
-		{
-			fSurname >> surname;
-		}
-		int randPatronymic = rand() % 20 + 1;
-		for (int i = 0; i < randPatronymic; i++)
-		{
-			fPatronymic >> patronymic;
-		}
+private:
+	int funcRandOperator(int mas[], int operatorN);
+	void sumOneNumb();
 
-		randOperator = (rand() % 10) % 3 + 1;
-		for (int i = 0; i < randOperator; i++) {
-			fcellOperator >> cellOperator;
-		}
-
-		switch (randOperator) {
-		case 1: operatorN = funcRandOperator(kyivstar, operatorN);
-			break;
-		case 2: operatorN = funcRandOperator(vodafon, operatorN);
-			break;
-		case 3: operatorN = funcRandOperator(life, operatorN);
-			break;
-		}
-		first = rand() % 900 + 100;
-		int i = 1000;
-		while (i >= 1) {
-			last += rand() % 10 * i;
-			i /= 10;
-		}
-
-		fName.close();
-		fSurname.close();
-		fPatronymic.close();
-		fcellOperator.close();
-
-		sumOneNumb();
-	}
-
-	void print() {
-		cout << num << ". " << surname << " " << name << " " << patronymic << endl
-			<< cellOperator << endl
-			<< "0" << operatorN << first << last << endl
-			<< "Сумма номера: " << sumOne << "\n\n";
-	}
-
-	void newNumber() {
-		if (previous == 0)
-		{
-			num = 1;
-		}
-		else {
-			num = previous->num + 1;
-		}
-	}
-
-	string printInFile() {
-		return to_string(num) + ". " + surname + " " + name + " " + patronymic + "\n"
-			+ cellOperator + "\n"
-			+ "0" + to_string(operatorN) + to_string(first) + to_string(last) + "\n"
-			+ "Сумма номера: " + to_string(sumOne);
-	}
-
-	string getName() {
-		return name;
-	}
-
-	string getSurname() {
-		return surname;
-	}
-
-	string getPatronymic() {
-		return patronymic;
-	}
-
-	string getCellOperator() {
-		return cellOperator;
-	}
 };
 
